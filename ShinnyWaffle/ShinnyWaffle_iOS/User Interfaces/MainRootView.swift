1f73bbc4733171ea6a82ba4980c0e60958ce3178
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 16:33:16 -0300

    Customize view

--- ShinnyWaffle/ShinnyWaffle_iOS/User Interfaces/MainRootView.swift
+++ ShinnyWaffle/ShinnyWaffle_iOS/User Interfaces/MainRootView.swift
@@ -10,9 +10,51 @@ import UIKit
 import ShinnyWaffleUIKit
 
 public class MainRootView: NiblessView {
+    private var hierarchyNotReady = true
+
+    let button: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let title = NSLocalizedString("Get quote", comment: "Get quote")
+        button.setTitle(title, for: .normal)
+
+        button.setTitleColor(UIColor.black, for: .normal)
+
+        return button
+    }()
+
     public override func didMoveToWindow() {
         super.didMoveToWindow()
 
-        self.backgroundColor = UIColor.red
+        guard hierarchyNotReady else {
+            return
+        }
+
+        self.constructHierarchy()
+        self.activateConstraints()
+
+        self.backgroundColor = UIColor.white
+
+        self.hierarchyNotReady = false
+    }
+}
+
+extension MainRootView { // MARK: - Helpers
+    fileprivate func constructHierarchy() {
+        self.addSubview(self.button)
+    }
+
+    private func activateButtonConstraints() {
+        let xConstraint = self.button.centerXAnchor.constraint(equalTo: layoutMarginsGuide.centerXAnchor)
+        let yConstraint = self.button.centerYAnchor.constraint(equalTo: layoutMarginsGuide.centerYAnchor)
+        let toActivate = [
+            xConstraint,
+            yConstraint
+        ]
+        NSLayoutConstraint.activate(toActivate)
+    }
+
+    fileprivate func activateConstraints() {
+        self.activateButtonConstraints()
     }
 }
