6a59122558e2857401d905f0aca0039449c71c65
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 16:09:56 -0300

    Add ShinnyWaffle_iOS

---
+++ ShinnyWaffle/ShinnyWaffle_iOS/ShinnyWaffle_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ShinnyWaffle_iOS.h
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShinnyWaffle_iOS.
+FOUNDATION_EXPORT double ShinnyWaffle_iOSVersionNumber;
+
+//! Project version string for ShinnyWaffle_iOS.
+FOUNDATION_EXPORT const unsigned char ShinnyWaffle_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShinnyWaffle_iOS/PublicHeader.h>
+
+
