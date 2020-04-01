8e78af9797a36def088641bd39b0a2a7e9568bb0
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 16:16:55 -0300

    Add AppDependencyContainer

---
+++ ShinnyWaffle/ShinnyWaffle_iOS/AppDependencyContainer.swift
@@ -0,0 +1,18 @@
+//
+//  AppDependencyContainer.swift
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+public struct AppDependencyContainer {
+    public func makeMainViewController() -> MainViewController {
+        let userInterface = MainRootView()
+        let mainViewController = MainViewController(userInterface: userInterface)
+
+        return mainViewController
+    }
+}
