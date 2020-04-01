3bb45bc78273e7e3064db0cbd7ee9c64ee76b1c9
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 16:14:23 -0300

    Add Main View Controller

---
+++ ShinnyWaffle/ShinnyWaffle_iOS/User Interfaces/MainRootView.swift
@@ -0,0 +1,22 @@
+//
+//  MainRootView.swift
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+import ShinnyWaffleUIKit
+
+public class MainRootView: NiblessView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ShinnyWaffle/ShinnyWaffle_iOS/View Controllers/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+import ShinnyWaffleUIKit
+
+public class MainViewController: NiblessViewController {
+    let userInterface: MainRootView
+
+    public init(userInterface: MainRootView) {
+        self.userInterface = userInterface
+
+        super.init()
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    public override func loadView() {
+        super.loadView()
+
+        self.view = self.userInterface
+    }
+}
