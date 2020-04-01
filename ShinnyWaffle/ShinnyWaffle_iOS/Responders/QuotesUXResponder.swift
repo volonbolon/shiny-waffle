912c9a6e99f1a4bb8a8d41223160145615d55088
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 20:17:55 -0300

    Present Quote

---
+++ ShinnyWaffle/ShinnyWaffle_iOS/Responders/QuotesUXResponder.swift
@@ -0,0 +1,9 @@
+//
+//  QuotesUXResponder.swift
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
---
+++ ShinnyWaffle/ShinnyWaffle_iOS/View Models/QuotestViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  QuotestViewModel.swift
+//  ShinnyWaffle_iOS
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
