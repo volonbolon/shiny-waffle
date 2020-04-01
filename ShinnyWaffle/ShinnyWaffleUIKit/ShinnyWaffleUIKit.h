5a87a185201db0776d703222197e6581f41107b2
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-01 16:07:53 -0300

    Add ShinnyWaffleUIKit target

---
+++ ShinnyWaffle/ShinnyWaffleUIKit/ShinnyWaffleUIKit.h
@@ -0,0 +1,19 @@
+//
+//  ShinnyWaffleUIKit.h
+//  ShinnyWaffleUIKit
+//
+//  Created by Ariel Rodriguez on 01/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShinnyWaffleUIKit.
+FOUNDATION_EXPORT double ShinnyWaffleUIKitVersionNumber;
+
+//! Project version string for ShinnyWaffleUIKit.
+FOUNDATION_EXPORT const unsigned char ShinnyWaffleUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShinnyWaffleUIKit/PublicHeader.h>
+
+
